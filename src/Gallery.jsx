ab560e5839aacb1c8e164a20ca161052011103d3
Selvam6994 <selvamyoor@gmail.com>
2023-03-28 18:11:39 +0530

    imported images path

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -3,64 +3,78 @@ import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { motion } from "framer-motion";
+import image1 from "../src/assets/Images/Image01.webp"
+import image2 from "../src/assets/Images/Image02.webp"
+import image3 from "../src/assets/Images/Image03.webp"
+import image4 from "../src/assets/Images/Image04.webp"
+import image5 from "../src/assets/Images/Image05.webp"
+import image6 from "../src/assets/Images/Image06.webp"
+import image7 from "../src/assets/Images/Image07.webp"
+import image8 from "../src/assets/Images/Image08.webp"
+import image9 from "../src/assets/Images/Image09.webp"
+import image10 from "../src/assets/Images/Image10.webp"
+import image11 from "../src/assets/Images/Image11.webp"
+import image12 from "../src/assets/Images/Image12.webp"
+import image13 from "../src/assets/Images/Image13.webp"
+import image14 from "../src/assets/Images/Image14.webp"
 
 function Gallery() {
   const images = [
     {
-      image: "/src/assets/Images/About us.webp",
+      image: image1,
       transDuration: 1,
     },
     {
-      image: "/src/assets/Images/Image02.webp",
+      image: image2,
       transDuration: 2,
     },
     {
-      image: "/src/assets/Images/Image03.webp",
-      transDuration: 3,
+        image: image3,
+        transDuration: 3,
     },
     {
-      image: "/src/assets/Images/Image04.webp",
-      transDuration: 4,
+        image: image4,
+        transDuration: 4,
     },
     {
-      image: "/src/assets/Images/Image05.webp",
-      transDuration: 5,
+        image: image5,
+        transDuration: 5,
     },
     {
-      image: "/src/assets/Images/Image06.webp",
-      transDuration: 6,
+        image: image6,
+        transDuration: 6,
     },
     {
-      image: "/src/assets/Images/Image07.webp",
-      transDuration: 7,
+        image: image7,
+        transDuration: 7,
     },
     {
-      image: "/src/assets/Images/Image08.webp",
-      transDuration: 8,
+        image: image8,
+        transDuration: 8,
     },
     {
-      image: "/src/assets/Images/Image09.webp",
-      transDuration: 9,
+        image: image9,
+        transDuration: 9,
     },
     {
-      image: "/src/assets/Images/Image10.webp",
-      transDuration: 10,
+        image: image10,
+        transDuration: 10,
     },
     {
-      image: "/src/assets/Images/Image11.webp",
-      transDuration: 11,
+        image: image11,
+        transDuration: 11,
     },
     {
-      image: "/src/assets/Images/Image12.webp",
-      transDuration: 12,
+        image: image12,
+        transDuration: 12,
     },
     {
-      image: "/src/assets/Images/Image13.webp",
-      transDuration: 13,
+        image: image13,
+        transDuration: 13,
     },
     {
-      image: "/src/assets/Images/Image14.webp",
-      transDuration: 14,
+        image: image14,
+        transDuration: 14,
     },
   ];
 
